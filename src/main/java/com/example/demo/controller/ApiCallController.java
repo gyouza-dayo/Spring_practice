bb0267de2dfc6e42ef176111a80202092a0806e4
gyouza-dayo <milknyu.mail@gmail.com>
2021-05-05 21:21:54 +0900

    add Thymeleaf

--- src/main/java/com/example/demo/controller/ApiCallController.java
+++ src/main/java/com/example/demo/controller/ApiCallController.java
@@ -1,193 +1,193 @@
-package com.example.demo.controller;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Properties;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.YouTube.Search;
-import com.google.api.services.youtube.model.ResourceId;
-import com.google.api.services.youtube.model.SearchListResponse;
-import com.google.api.services.youtube.model.SearchResult;
-import com.google.api.services.youtube.model.Thumbnail;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-
-
-@RestController
-@RequestMapping("/youtube")
-public class ApiCallController {
-	/** Global instance properties filename. */
-	private static String PROPERTIES_FILENAME = "youtube.properties";
-
-	/** Global instance of the HTTP transport. */
-	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-
-	/** Global instance of the JSON factory. */
-	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
-
-	/**
-	 * Global instance of the max number of videos we want returned (50 = upper
-	 * limit per page).
-	 */
-	private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
-
-	/** Global instance of Youtube object to make all API requests. */
-	private static YouTube youtube;
-
-	@RequestMapping(value = "/youtube/")
-	private String call() {
-		return "";
-	}
-
-	/**
-	 * Initializes YouTube object to search for videos on YouTube
-	 * (Youtube.Search.List). The program then prints the names and thumbnails of
-	 * each of the videos (only first 50 videos).
-	 *
-	 * @param args command line args.
-	 */
-	public static void main(String[] args) {
-		// youtube.propertiesから開発者キーを読む
-		Properties properties = new Properties();
-		try {
-			InputStream in = Search.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
-			properties.load(in);
-
-		} catch (IOException e) {
-			System.err.println(
-					"There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause() + " : " + e.getMessage());
-			System.exit(1);
-		}
-
-		try {
-			/*
-			 * すべてのAPIリクエストには、YouTubeオブジェクトが使用されます。
-			 * 最後の引数は必須ですが、HttpRequestの初期化時に何も初期化する必要がないため、
-			 * インターフェースをオーバーライドして、no-op関数(何もしない)を提供しています。
-			 */
-			youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
-				public void initialize(HttpRequest request) throws IOException {
-				}
-			}).setApplicationName("youtube-cmdline-search-sample").build();
-
-			// ユーザーからのクエリ用語(検索文字列)の取得.
-			String queryTerm = getInputQuery();
-
-			/*
-			 * youtube.search().list("id,snippet")のエラー
-			 * The method list(List<String>) in the type YouTube.Search is not applicable for the arguments (String)
-			 * の解消の為に、一時的にList<String>を引数に渡してみる
-			 */
-			List<String> idSnippetList = new ArrayList<>();
-			idSnippetList.add("id");
-			idSnippetList.add("snippet");
-			YouTube.Search.List search = youtube.search().list(idSnippetList);
-			
-			/*
-			 * 認証されていないリクエストでは、
-			 * Google Developer Console で API キーを設定することが重要です
-			 * (次のリンクの Credentials タブにあります: console.developers.google.com/)
-			 * これは良い習慣であり、あなたのクォータを増加させます。
-			 */
-			String apiKey = properties.getProperty("youtube.apikey");
-			search.setKey(apiKey);
-			search.setQ(queryTerm);
-			
-			/*
-			 * We are only searching for videos (not playlists or channels). If we were
-			 * searching for more, we would add them as a string like this:
-			 * "video,playlist,channel".
-			 */
-			search.setType("video");
-			/*
-			 * This method reduces the info returned to only the fields we need and makes
-			 * calls more efficient.
-			 */
-			search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-			search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
-			SearchListResponse searchResponse = search.execute();
-
-			List<SearchResult> searchResultList = searchResponse.getItems();
-
-			if (searchResultList != null) {
-				prettyPrint(searchResultList.iterator(), queryTerm);
-			}
-		} catch (GoogleJsonResponseException e) {
-			System.err.println(
-					"There was a service error: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
-		} catch (IOException e) {
-			System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-	}
-
-	/*
-	 * 端末を介してユーザーが入力したクエリ用語（文字列）を返す
-	 * サンプルではコンソールに入力を受け付ける。
-	 */
-	private static String getInputQuery() throws IOException {
-
-		String inputQuery = "";
-
-		System.out.print("Please enter a search term: ");
-		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
-		inputQuery = bReader.readLine();
-
-		if (inputQuery.length() < 1) {
-			// 何も入力されていない場合は、デフォルトで "YouTube Developers Live "と表示されます。
-			inputQuery = "YouTube Developers Live";
-		}
-		return inputQuery;
-	}
-
-	/*
-	 * Prints out all SearchResults in the Iterator. Each printed line includes
-	 * title, id, and thumbnail.
-	 *
-	 * @param iteratorSearchResults Iterator of SearchResults to print
-	 *
-	 * @param query Search query (String)
-	 */
-	private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
-
-		System.out.println("\n=============================================================");
-		System.out.println("   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
-		System.out.println("=============================================================\n");
-
-		if (!iteratorSearchResults.hasNext()) {
-			System.out.println(" There aren't any results for your query.");
-		}
-
-		while (iteratorSearchResults.hasNext()) {
-
-			SearchResult singleVideo = iteratorSearchResults.next();
-			ResourceId rId = singleVideo.getId();
-
-			// Double checks the kind is video.
-			if (rId.getKind().equals("youtube#video")) {
-				Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().get("default");
-
-				System.out.println(" Video Id" + rId.getVideoId());
-				System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
-				System.out.println(" Thumbnail: " + thumbnail.getUrl());
-				System.out.println("\n-------------------------------------------------------------\n");
-			}
-		}
-	}
-}
+//package com.example.demo.controller;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.List;
+//import java.util.Properties;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+//import com.google.api.client.http.HttpRequest;
+//import com.google.api.client.http.HttpRequestInitializer;
+//import com.google.api.client.http.HttpTransport;
+//import com.google.api.client.http.javanet.NetHttpTransport;
+//import com.google.api.client.json.JsonFactory;
+//import com.google.api.client.json.jackson2.JacksonFactory;
+//import com.google.api.services.youtube.YouTube;
+//import com.google.api.services.youtube.YouTube.Search;
+//import com.google.api.services.youtube.model.ResourceId;
+//import com.google.api.services.youtube.model.SearchListResponse;
+//import com.google.api.services.youtube.model.SearchResult;
+//import com.google.api.services.youtube.model.Thumbnail;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.Iterator;
+//
+//
+//@RestController
+//@RequestMapping("/youtube")
+//public class ApiCallController {
+//	/** Global instance properties filename. */
+//	private static String PROPERTIES_FILENAME = "youtube.properties";
+//
+//	/** Global instance of the HTTP transport. */
+//	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+//
+//	/** Global instance of the JSON factory. */
+//	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+//
+//	/**
+//	 * Global instance of the max number of videos we want returned (50 = upper
+//	 * limit per page).
+//	 */
+//	private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+//
+//	/** Global instance of Youtube object to make all API requests. */
+//	private static YouTube youtube;
+//
+//	@RequestMapping(value = "/youtube/")
+//	private String call() {
+//		return "";
+//	}
+//
+//	/**
+//	 * Initializes YouTube object to search for videos on YouTube
+//	 * (Youtube.Search.List). The program then prints the names and thumbnails of
+//	 * each of the videos (only first 50 videos).
+//	 *
+//	 * @param args command line args.
+//	 */
+//	public static void main(String[] args) {
+//		// youtube.propertiesから開発者キーを読む
+//		Properties properties = new Properties();
+//		try {
+//			InputStream in = Search.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
+//			properties.load(in);
+//
+//		} catch (IOException e) {
+//			System.err.println(
+//					"There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause() + " : " + e.getMessage());
+//			System.exit(1);
+//		}
+//
+//		try {
+//			/*
+//			 * すべてのAPIリクエストには、YouTubeオブジェクトが使用されます。
+//			 * 最後の引数は必須ですが、HttpRequestの初期化時に何も初期化する必要がないため、
+//			 * インターフェースをオーバーライドして、no-op関数(何もしない)を提供しています。
+//			 */
+//			youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+//				public void initialize(HttpRequest request) throws IOException {
+//				}
+//			}).setApplicationName("youtube-cmdline-search-sample").build();
+//
+//			// ユーザーからのクエリ用語(検索文字列)の取得.
+//			String queryTerm = getInputQuery();
+//
+//			/*
+//			 * youtube.search().list("id,snippet")のエラー
+//			 * The method list(List<String>) in the type YouTube.Search is not applicable for the arguments (String)
+//			 * の解消の為に、一時的にList<String>を引数に渡してみる
+//			 */
+//			List<String> idSnippetList = new ArrayList<>();
+//			idSnippetList.add("id");
+//			idSnippetList.add("snippet");
+//			YouTube.Search.List search = youtube.search().list(idSnippetList);
+//			
+//			/*
+//			 * 認証されていないリクエストでは、
+//			 * Google Developer Console で API キーを設定することが重要です
+//			 * (次のリンクの Credentials タブにあります: console.developers.google.com/)
+//			 * これは良い習慣であり、あなたのクォータを増加させます。
+//			 */
+//			String apiKey = properties.getProperty("youtube.apikey");
+//			search.setKey(apiKey);
+//			search.setQ(queryTerm);
+//			
+//			/*
+//			 * We are only searching for videos (not playlists or channels). If we were
+//			 * searching for more, we would add them as a string like this:
+//			 * "video,playlist,channel".
+//			 */
+//			search.setType("video");
+//			/*
+//			 * This method reduces the info returned to only the fields we need and makes
+//			 * calls more efficient.
+//			 */
+//			search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+//			search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+//			SearchListResponse searchResponse = search.execute();
+//
+//			List<SearchResult> searchResultList = searchResponse.getItems();
+//
+//			if (searchResultList != null) {
+//				prettyPrint(searchResultList.iterator(), queryTerm);
+//			}
+//		} catch (GoogleJsonResponseException e) {
+//			System.err.println(
+//					"There was a service error: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+//		} catch (IOException e) {
+//			System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+//		} catch (Throwable t) {
+//			t.printStackTrace();
+//		}
+//	}
+//
+//	/*
+//	 * 端末を介してユーザーが入力したクエリ用語（文字列）を返す
+//	 * サンプルではコンソールに入力を受け付ける。
+//	 */
+//	private static String getInputQuery() throws IOException {
+//
+//		String inputQuery = "";
+//
+//		System.out.print("Please enter a search term: ");
+//		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+//		inputQuery = bReader.readLine();
+//
+//		if (inputQuery.length() < 1) {
+//			// 何も入力されていない場合は、デフォルトで "YouTube Developers Live "と表示されます。
+//			inputQuery = "YouTube Developers Live";
+//		}
+//		return inputQuery;
+//	}
+//
+//	/*
+//	 * Prints out all SearchResults in the Iterator. Each printed line includes
+//	 * title, id, and thumbnail.
+//	 *
+//	 * @param iteratorSearchResults Iterator of SearchResults to print
+//	 *
+//	 * @param query Search query (String)
+//	 */
+//	private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+//
+//		System.out.println("\n=============================================================");
+//		System.out.println("   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
+//		System.out.println("=============================================================\n");
+//
+//		if (!iteratorSearchResults.hasNext()) {
+//			System.out.println(" There aren't any results for your query.");
+//		}
+//
+//		while (iteratorSearchResults.hasNext()) {
+//
+//			SearchResult singleVideo = iteratorSearchResults.next();
+//			ResourceId rId = singleVideo.getId();
+//
+//			// Double checks the kind is video.
+//			if (rId.getKind().equals("youtube#video")) {
+//				Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().get("default");
+//
+//				System.out.println(" Video Id" + rId.getVideoId());
+//				System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
+//				System.out.println(" Thumbnail: " + thumbnail.getUrl());
+//				System.out.println("\n-------------------------------------------------------------\n");
+//			}
+//		}
+//	}
+//}
--- src/main/java/com/example/demo/controller/SampleApiController.java
+++ src/main/java/com/example/demo/controller/SampleApiController.java
@@ -1,10 +1,18 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
+
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 /*
@@ -12,27 +20,98 @@ import org.springframework.web.client.RestTemplate;
  * cityパラメータ/地域のid を加えてアクセスして使用する
  */
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class SampleApiController {
-	
-	@RequestMapping(value="/weather/sagami",
-			produces=MediaType.APPLICATION_JSON_VALUE,
-			method=RequestMethod.GET)
-	public String call() {
-		
-		RestTemplate rest = new RestTemplate(); //APIを使う為のインスタンス
-		final String cityCode = "140020"; //今回のAPI用の、URIに追加する文字列
-		final String endPoint = "https://weather.tsukumijima.net/api/forecast"; //今回のAPIの基本URI
-
-		//https://weather.tsukumijima.net/api/forecast?city=140020 となる
-		final String url = endPoint + "?city=" + cityCode;
-		
-		//RestTemplateインスタンスの .getForEntityメソッドに作成したURIを渡し、
-		//その結果を ResponseEntity<E> オブジェクトに受け取る。
-		ResponseEntity<String> response = rest.getForEntity(url, String.class);
-		
-		
-		
-		return null;
+	// 外部のAPIをコールするHTTPクライアント RestTemplate をインスタンス化
+	private final RestTemplate rest;
+	{
+		//使用するRestTemplateインスタンスのErrorHandlerプロパティに自前のエラーハンドラを設定
+		this.rest = new RestTemplate();
+		this.rest.setErrorHandler(new QuietlyHandler());
 	}
+	
+	@RequestMapping(value = "/weather/sagami", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+	public String call() {
+		// APIの共通URI
+		final String endPoint = "https://weather.tsukumijima.net/api/forecast";
+		// URIのパラメータ(city)に指定するコード。 欲しいリソースによって変わる
+		final String cityCode = "140020";
+		
+		// https://weather.tsukumijima.net/api/forecast?city=140020 となる
+		final String url = endPoint + "?city=" + cityCode;
+
+		// RestTemplateインスタンスの .getForEntityメソッドに作成したURIを渡し、
+		// その結果を ResponseEntity<E> オブジェクトに受け取る。
+		// 第二引数にStringで内容を受け取るように指定しているが、Beanを指定して直接マップも可能！
+		ResponseEntity<String> response = rest.getForEntity(url, String.class);
+
+		// responseの中身をJSON形式に変換
+		String json = response.getBody();
+
+		return decode(json);
+	}
+	
+	//これを使用しなかった場合、JSON文字列に \n を含んだそのまま出力されるが、
+	//StringEscapeUtilsを用いると \n を改行して出力してくれた。
+	private static String decode(String string) {
+		return StringEscapeUtils.unescapeJava(string);
+	}
+	
+	/**
+	* POST実装例.
+	* 
+	* 指定したエンドポイントに対して {@code json} データをPOSTし、結果を返す。
+	* 
+	* @param url     エンドポイント
+	* @param headers リクエストヘッダ
+	* @param json    送信するJSON文字列
+	* @return 正常に通信出来た場合はレスポンスのJSON文字列を、<br>
+	*         正常に通信出来なかった場合は {@code null} を返す。 
+	*/
+	public String post(String url, Map<String, String> headers, String json) {
+		//リクエストエンティティに詳細な情報を設定出来るBodyBuilderインスタンスを生成
+		//RequestEntityでは、各HTTPメソッドに対応したBodyBuilderの生成APIが提供されている
+		RequestEntity.BodyBuilder builder = RequestEntity.post(uri(url));
+		
+		//引数に受け取ったヘッダ情報をBodyBuilderインスタンスにセットする
+		for(String name : headers.keySet()) {	//name に keyを取得
+			String header = headers.get(name);	//header に valueを取得
+			builder.header(name,header);			//BodyBuilderインスタンスに一行分のヘッダ情報を追加
+		}
+		
+		//作成したBodyBuilderインスタンスから、RequestEntityインスタンスを生成
+		RequestEntity<String> request = builder
+				.contentType(MediaType.APPLICATION_JSON_UTF8)	//MediaTypeをセット
+				.body(json);															//送信するJSONクエリをセット
+		
+		//RestTemplate.exchange()に、作成したRequestEntityとresponseTypeを指定して
+		//ResponseEntityインスタンスを生成
+		ResponseEntity<String> response = this.rest.exchange(
+				request,
+				String.class);
+		
+		//レスポンスからHTTPステータスコードを取得し、
+		//is2xxSuccessfulで通信の成功を示す200番台のコードであるか？の判定を行う。
+		//判定がtrueで通信に成功していれば取得したデータ本体、失敗していればnullを返す。
+		return response.getStatusCode().is2xxSuccessful() ? response.getBody() : null;
+	}
+	
+	//URL文字列をURIオブジェクトに変換する
+	private static final URI uri(String url) {
+		try {
+			return new URI(url);
+		}catch(Exception ex) {
+			throw new RuntimeException( ex );
+		}
+	}
+	
+	private static class QuietlyHandler extends DefaultResponseErrorHandler{
+		@Override
+		public void handleError(ClientHttpResponse response) throws IOException{
+			//4xx系、5xx系のステータスコードであっても何もしない(NOP処理)
+		}		
+	}
+	
+
 }
